9378f114480d841da612878d4b714a493f755b17
boytur <104257779+boytur@users.noreply.github.com>
2024-08-11 20:32:32 +0700

    ✨ feat[responseMiddleware]: added supprt meta data

--- src/middleware/responseMiddleware.ts
+++ src/middleware/responseMiddleware.ts
@@ -1,9 +1,13 @@
-
 import { Request, Response, NextFunction } from "express";
 
 export interface CustomResponse extends Response {
-  customSuccess: (data: any, status: number, message?: string) => void;
-  customError: (message: string, status: number) => void;
+  customSuccess: (data: any, status: number, message?: string, metaData?: {
+    page: number;
+    totalPages: number;
+    totalCount: number;
+    perPage: number;
+  }) => void;
+  customError: (message: string, status: number, errors?: any[]) => void;
 }
 
 /**
@@ -20,23 +24,31 @@ export const responseMiddleware = (
   (res as CustomResponse).customSuccess = (
     data: any,
     status: number,
-    message?: string
+    message?: string,
+    metaData?: {
+      page: number;
+      totalPages: number;
+      totalCount: number;
+      perPage: number;
+    },
   ) => {
     (res as CustomResponse).status(status).json({
       success: true,
       message: message || "Operation successful",
       data,
+      metaData
     });
   };
 
   (res as CustomResponse).customError = (
     message: string,
     status: number,
-    errors?: string
+    errors?: any[]
   ) => {
     (res as CustomResponse).status(status).json({
       success: false,
       message,
+      errors,
     });
   };
 
