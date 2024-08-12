930af38b9c2b5fc1d4b6525fd9a781335262a265
PSBLaLaHey <pizz12547@gmail.com>
2024-08-12 17:45:58 +0700

    ✨feat[user.routes.ts]: use auth Middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,9 +3,11 @@ import { UserController } from "../controllers/user.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { getUsersValidationRules, userValidationRules } from "../utils/validators";
 import { validate } from "../middleware/validate";
+import { TodoController } from "../controllers/todo.controller";
 
 const router = Router();
 const userController = new UserController();
+const todoController = new TodoController();
 
 router.post(
   "/users",
@@ -18,6 +20,7 @@ router.get(
   "/users", 
   getUsersValidationRules(), 
   validate, 
+  authMiddleware,
   userController.getUsers
 );
 
